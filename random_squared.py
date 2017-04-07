6aa64bcb53fb9185476ba0c1f850015a18d887ea
Harper Frankstone <Harper Frankstone>
2017-04-07 15:45:42 -0500

    the squared list of numbers is now complete

--- random_squared.py
+++ random_squared.py
@@ -4,5 +4,8 @@ random_numbers = list()
 for i in range (20):
 	random_numbers.append(random.randrange(0, 49, 2))
 
-print(len(random_numbers))
-print(random_numbers)
+print(len(random_numbers)) #making sure that the list has 20 numbers in it
+print(random_numbers) #the list of random numbers
+
+square_list = [i**2 for i in random_numbers] #the double asterisk is the squaring math function
+print(square_list)
